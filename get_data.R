7369781a4888c15de0f2fb9a7a75b9bf437e237e
ben weiher <bweiher@pm.me>
2020-03-27 01:16:22 -0700

    cfirst commit

---
+++ get_data.R
@@ -0,0 +1,103 @@
+library(tidyverse)
+library(lubridate)
+library(glue)
+
+
+# generate sequence of dates from first date data is avail to now
+ds <- seq.Date(ymd(20200122),Sys.Date(),by='days')
+
+
+# read in CSVs from this github repo 
+data <- map(ds, function(x){
+  
+  #Sys.sleep(1)
+  
+  print(x)
+  
+  ds_formatted <- format(x, "%m-%d-%Y" )
+  
+  url <- glue("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/{ds_formatted}.csv")
+  
+  
+  tryCatch( read_csv(url) %>% 
+    mutate(
+      ds = x
+    ) ,
+    error=  function(e) NA
+  )
+  
+  
+}) %>% 
+  .[!is.na(.)] # drop NAs incase data isn't here for current DS
+
+
+data <- data[!is.na(data)]
+
+
+# format data since it changes over time
+df <- map_df(1:length(data), function(x){
+  
+  tmp <- data[[x]]
+  print(x)
+  
+  if("Province_State" %in% colnames(tmp)){
+    select(
+      tmp ,
+      country  =  Country_Region ,
+      province = Province_State,
+      lat = Lat, 
+      lon = Long_ ,
+      confirmed = Confirmed ,
+      active = Active ,
+      deaths = Deaths ,
+      recovered = Recovered,
+      ds 
+    )
+    
+  } else {
+    
+    select(
+      tmp ,
+      country  =  `Country/Region` ,
+      province = `Province/State`,
+      lat = NA_real_, 
+      lon = NA_real_ ,
+      confirmed = Confirmed ,
+      active = NA_real_ ,
+      deaths = Deaths ,
+      recovered = Recovered,
+      ds
+    )
+  }
+  
+  
+}) 
+
+
+# imputations for changing names of some countries ...
+df <- df %>% 
+  mutate(
+    country =  case_when(
+      str_detect(country, "Iran") ~ "Iran",
+      str_detect(country, "Republic of Korea|South Korea|Korea, South") ~ "South Korea",
+      str_detect(country, "Mainland China|China") ~ "China",
+      TRUE ~ country
+    ),
+    province = case_when(
+      country=='US' & province %like% ", CA" ~ "California",
+      country=='US' & province %like% ", WA" ~ "Washington",
+      country=='US' & province %like% ", NY" ~ "New York",
+      country=='US' & province %like% ", NJ" ~ "New Jersey",
+      country=='US' & province %like% ", IL" ~ "Illinois",
+      country=='US' & province %like% ", WA" ~ "Washington",
+      country=='US' & province %like% ", OR" ~ "Oregon",
+      country=='US' & province %like% ", TX" ~ "Texas",
+      country=='US' & province %like% ", CO" ~ "Colorado",
+      country=='US' & province %like% ", AZ" ~ "Arizona",
+      TRUE ~ province
+    )
+  ) %>% 
+  arrange(country,province,ds)
+
+
+write_csv(df, "data/data.csv")
---
+++ make_plots.R
@@ -0,0 +1,134 @@
+library(tidyverse)
+library(lubridate)
+library(glue)
+library(highcharter)
+options(highcharter.theme = bwmisc::theme_ipsum())
+
+df <- read_csv("data/data.csv")
+
+# find the top N countries with by their max-confirmed
+top <- df %>% 
+  group_by(country , ds) %>% 
+  summarise(confirmed = sum(confirmed)) %>% 
+  filter(confirmed == max(confirmed, na.rm=TRUE)) %>% 
+  arrange(-confirmed, country, -desc(ds)) %>% 
+  ungroup() %>% 
+  slice(1:10) %>% 
+  pull(country)
+
+
+
+# find each countries 100 cases ds
+by_country <- df %>% 
+  group_by(country,ds) %>% 
+  summarise(
+    confirmed = sum(confirmed, na.rm = TRUE)
+  ) %>% 
+  ungroup()
+  
+  
+tipping_points <- by_country %>% 
+  filter(country %in% top) %>% 
+  filter(confirmed >= 100) %>% 
+  distinct(country,ds) %>% 
+  select(country, first_ds = ds) %>% 
+  group_by(country) %>% 
+  filter(first_ds ==  min(first_ds)) %>% 
+  ungroup()
+
+
+
+by_country %>% 
+  filter(country %in% top) %>% 
+  left_join(tipping_points) %>% 
+  filter(ds >= first_ds) %>% 
+  group_by(country) %>% 
+  mutate(rn =  row_number()) %>%
+  hchart("line", hcaes(
+    x = rn, 
+    y = confirmed,
+    group = country,
+    name = country
+  )) %>% 
+  hc_title(text="Confirmed COVID-19 Cases by Country") %>% 
+  hc_subtitle(text='By Days Since Country Reached 100 Cases')
+
+
+
+# look at within the US 
+
+us <- df %>% 
+  filter(country == 'US') %>% 
+  group_by(province,ds) %>% 
+  summarise(confirmed=sum(confirmed, na.rm=TRUE))
+
+# see if the states look right ...
+ us %>% 
+   group_by(province) %>% 
+   summarise(n=sum(confirmed)) %>% 
+   arrange(-n) %>% 
+   print(n=Inf)
+ 
+ 
+
+top_states <- us %>% 
+  filter(confirmed==max(confirmed)) %>% 
+  arrange(-confirmed) %>% 
+  head(7) %>% 
+  ungroup() %>%
+  pull(province)
+
+
+states_tipping_point <- us %>% 
+  filter(confirmed >= 100) %>% 
+  group_by(province) %>% 
+  filter(ds == min(ds)) %>% 
+  ungroup() %>% 
+  select(province, min_ds  = ds)
+
+
+  
+us %>% 
+  inner_join(states_tipping_point) %>% 
+  filter( ds >= min_ds) %>% 
+  filter(province %in% top_states) %>% 
+  group_by(province) %>% 
+  mutate(rn=row_number()) %>% 
+  ggplot(aes(
+    x = rn, 
+    y = confirmed,
+    group = province ,
+    color = province
+  )) +
+  geom_line() +
+  hrbrthemes::theme_ipsum() +
+  theme(legend.position = 'bottom') +
+  labs(
+    x = "Day Since 100 Cases",
+    y="Confirmed Cases",
+    title="Confirmed COVID-19 Cases by State",
+    subtitle='By Days Since State Reached 100 Cases'
+  )
+
+
+
+us %>% 
+  inner_join(states_tipping_point) %>% 
+  filter( ds >= min_ds) %>% 
+  filter(province %in% top_states) %>% 
+  group_by(province) %>% 
+  mutate(rn=row_number()) %>% 
+  hchart("line", hcaes(
+    x = rn, 
+    y = confirmed,
+    group = province,
+    name = province
+  )) %>% 
+  hc_title(text="Confirmed COVID-19 Cases by State") %>% 
+  hc_subtitle(text='By Days Since State Reached 100 Cases') %>% 
+  hc_xAxis(crosshair=TRUE,
+           title= list(text="<i><b>Day Since 100 Cases")) %>% 
+  hc_yAxis(
+           title= list(text="<i><b> Confirmed Cases"))
+
+  
